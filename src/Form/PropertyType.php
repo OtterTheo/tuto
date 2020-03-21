f9ba1fe8b0e2a9135f22671d9ecd1c9e75dd4fb0
Theo <theonicolas19@outlook.com>
2020-03-21 10:44:38 +0100

    feat: validation

---
+++ src/Form/PropertyType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('hea', ChoiceType::class, [
+                'choices' => $this->getChoices()
+            ])
+            ->add('city')
+            ->add('address')
+            ->add('postal_code')
+            ->add('sold')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+
+    private function getChoices()
+    {
+        //méthode qui permet d'inverser la clé et valeur car si on fait afficher le tableau comme ça 0 => Gaz, la clé est 0 donc affichera 0 et la on inverse, on prend
+        //la valeur comme clé
+        $choices = Property::HEAT;
+        $output = [];
+        foreach ($choices as $k => $v){
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+}
